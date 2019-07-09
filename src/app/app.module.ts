d3f6882a11950f3724159132acfc8b47203dff88
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2019-07-08 23:28:22 -0300

    Interfaces, components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/User';
 
 @Component({
     selector: 'app-user',
@@ -7,39 +9,22 @@ import { Component } from '@angular/core';
     // stylesUrl: ['path']
 })
 
-export class UserComponent {
-    firstName: string = "Bruno";
-    lastName: string = 'Dev09';
-    age: number = 22;
-    address;
-
-    foo: any;
-    hasJob: boolean = true;
-    numberArr: number[] = [1,2,3];
-    strArr: string[] = ['hey', 'guys'];
-    mixedArrImportant: any[] = ['hey', 2, true];
-    myTuple: [string, number, boolean];
-    unusable: void;
-    und: undefined;
-    n: null;
+export class UserComponent implements OnInit {
+    user: User;
 
     constructor() {
-        this.age++;
-        this.address = {
-            street: "whatever st"
+    }
+    
+    ngOnInit() {
+        this.user = {
+            firstName: 'Bruno',
+            lastName: 'Dev09',
+            age: 22,
+            address: {
+                street: "wtv",
+                city: 'SP'
+            }
         }
     }
-
-    showAge() {
-        return this.age;
-    }
-
-    sayStr(str) {
-        console.log(`Hey ${str}`);
-        console.log(`Hey ${this.firstName}`);
-    }
-
-    basicArit(num1:number, num2:number): number {
-        return num1 + num2;
-    }
 }
+
