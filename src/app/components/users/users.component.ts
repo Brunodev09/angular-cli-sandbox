f8a5c3f2ab76bd6b788298f2b91abaaccacf3edc
Brunodev09 <bruno.mayol.giannotti@gmail.com>
2019-07-08 23:44:21 -0300

    Template syntax, *ngFor -> just like ng-repeat

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
   ngOnInit() {
+    this.users = [
+      {
+        firstName: 'Bruno',
+        lastName: 'Dev09',
+        age: 22,
+        address: {
+          street: "wtv",
+          city: 'SP'
+        }
+      },
+      {
+        firstName: 'Dino',
+        lastName: 'Nino',
+        age: 2,
+        address: {
+          street: "wtv",
+          city: 'SP'
+        }
+      },
+      {
+        firstName: 'Maysa',
+        lastName: 'Salton',
+        age: 23,
+        address: {
+          street: "wtv",
+          city: 'SP'
+        }
+      }
+    ];
+
+    this.addUser({
+      firstName: 'Added',
+      lastName: 'UserSuper',
+      age: 223133,
+      address: {
+        street: "wtv",
+        city: 'Mars'
+      }
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
